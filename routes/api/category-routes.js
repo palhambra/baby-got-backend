60bb983e6d95b29926f8f07b9f3a9749c263c696
Paolo <paolo.alhambra@yahoo.com>
2023-04-02 18:24:34 -0700

    Updated tag-routes and product-routes. Verified operating properly

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -70,7 +70,7 @@ router.delete('/:id', async (req, res) => {
       }
   });
   if (!categoryData) {
-    res.status(404).json({ message: 'No reader found with that id!' });
+    res.status(404).json({ message: 'No category found with that id' });
     return;
   }
   res.status(200).json(categoryData);
